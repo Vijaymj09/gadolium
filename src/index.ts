85799d7b3f37627380a82553a3f7204f03181831
Vijaymj09 <karthimj09@gmail.com>
2025-03-25 11:27:06 +0530

    add start script and restructure authentication routes; remove old migration and create new user table

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import "dotenv/config";
-import { hono } from "./routes";
 import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+
+const hono = new Hono();
 
 serve(hono, (info) => {
   console.log(`Server is running @ http://localhost:${info.port}`);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,13 @@
 import { Hono } from "hono";
-import { SignUpWithUsernameAndPasswordError } from "../controllers/+type";
-import { signUpWithUsernameAndPassword } from "../controllers";
-
+import { serve } from "@hono/node-server";
+import {
+  LogInWtihUsernameAndPasswordError,
+  SignUpWithUsernameAndPasswordError,
+} from "../controllers/+type";
+import {
+  signUpWithUsernameAndPassword,
+  logInWithUsernameAndPassword,
+} from "../controllers";
 
 export const hono = new Hono();
 
@@ -9,33 +15,15 @@ hono.post("/authentication/sign-up", async (context) => {
   const { username, password } = await context.req.json();
 
   try {
-    const result = await signUpWithUsernameAndPassword({
-      username,
-      password,
-    });
+    const result = await signUpWithUsernameAndPassword({ username, password });
 
-    return context.json(
-      {
-        data: result,
-      },
-      201
-    );
+    return context.json({ data: result }, 201);
   } catch (e) {
     if (e === SignUpWithUsernameAndPasswordError.CONFLICTING_USERNAME) {
-      return context.json(
-        {
-          message: "Username already exists",
-        },
-        409
-      );
+      return context.json({ message: "Username already exists" }, 409);
     }
 
-    return context.json(
-      {
-        mesage: "Unknown",
-      },
-      500
-    );
+    return context.json({ message: "Unknown error" }, 500);
   }
 });
 
@@ -43,41 +31,23 @@ hono.post("/authentication/log-in", async (context) => {
   try {
     const { username, password } = await context.req.json();
 
-    const result = await signUpWithUsernameAndPassword({
-      username,
-      password,
-    });
+    const result = await logInWithUsernameAndPassword({ username, password });
 
-    return context.json(
-      {
-        data: result,
-      },
-      201
-    );
+    return context.json({ data: result }, 201);
   } catch (e) {
-    if (e === SignUpWithUsernameAndPasswordError.INCORRECT_USERNAME_OR_PASSWORD) {
-      return context.json(
-        {
-          message: "Incorrect username or password",
-        },
-        401
-      );
+    if (e === LogInWtihUsernameAndPasswordError.INCORRECT_USERNAME_OR_PASSWORD) {
+      return context.json({ message: "Incorrect username or password" }, 401);
     }
 
-    return context.json(
-      {
-        message: "Unknown",
-      },
-      500
-    );
+    return context.json({ message: "Unknown error" }, 500);
   }
 });
 
 hono.get("/health", (context) => {
-  return context.json(
-    {
-      message: "All Ok",
-    },
-    200
-  );
+  return context.json({ message: "All Ok" }, 200);
+});
+
+// Start the server
+serve(hono, (info) => {
+  console.log(`Server is running on http://localhost:${info.port}`);
 });
